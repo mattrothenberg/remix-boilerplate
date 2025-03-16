a6702b4f8c5e4d95f6bac2fb7377e3fe2def8151
Matt Rothenberg <mattrothenberg@gmail.com>
2025-03-16 18:18:26 -0400

    pass env vars

--- app/root.tsx
+++ app/root.tsx
@@ -1,31 +1,46 @@
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useLoaderData,
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
 
+export async function loader() {
+	return {
+		env: {
+			SOME_ENV_VAR: process.env.SOME_ENV_VAR,
+		},
+	};
+}
+
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-        <Analytics />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				{children}
+				<ScrollRestoration />
+				<Scripts />
+				<Analytics />
+			</body>
+		</html>
+	);
 }
 
 export default function App() {
-  return <Outlet />;
+	const { env } = useLoaderData<typeof loader>();
+	return (
+		<div>
+			{JSON.stringify(env, null, 2)}
+			<Outlet />
+		</div>
+	);
 }
